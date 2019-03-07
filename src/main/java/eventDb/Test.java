19d4ad479a95dae68c26656788cbc7783303754d
spartan2015 <vasile.irimia@aurea.com>
2019-03-07 14:19:08 +0200

    test

---
+++ src/main/java/eventDb/Test.java
@@ -0,0 +1,35 @@
+package eventDb;
+
+import com.sun.javafx.event.EventUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Test {
+
+    public static void main(String... args) throws SQLException {
+
+        Connection connection = null;
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("select");
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+
+            resultSet.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally {
+            //closeStatement(resultSet);
+        }
+
+    }
+
+    private static void closeStatement(ResultSet resultSet) throws SQLException {
+        //resultSet.close();
+    }
+
+}
